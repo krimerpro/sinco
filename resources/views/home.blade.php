c728ab7b15e39ee41e69de92704640f22062bdf0
varanustoken <varanustoken@gmail.com>
2023-02-26 09:37:40 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
           <p class="text-lg pt-25 pb-40 lg-pb-20 sm-pt-10">Sinco delivered blazing fast, striking ai solution</p>
           <ul class="style-none button-group d-sm-flex align-items-center">
             <li class="me-4 mt-10"><a href="https://t.me/SincoAIBsc" class="btn-one ripple-btn">JOIN US</a></li>
-            <li><a class="demo-btn tran3s mt-10" href="https://poocoin.app/tokens/0xBeA5a9729bffB5427367893bF97884CDa49d4957">POOCOIN CHART</a></li>
+            <li><a class="demo-btn tran3s mt-10" href="https://poocoin.app/tokens/0x58a6ceb98f53785bc51bd2b32de6ee92c8f50ef3">POOCOIN CHART</a></li>
           </ul>
         </div>
       </div>
